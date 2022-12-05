9475390c3124bdd58292135ccd12ce0f2df1d9df
20btcs037hy <115235361+20btcs037hy@users.noreply.github.com>
2022-12-06 01:31:58 +0530

    Add files via upload

---
+++ # Arithmatic Calculator.py
@@ -0,0 +1,14 @@
+# Arithmatic Calculator
+
+num_1 = input('Enter First Number : ')
+num_2 = input('Enter Second Number : ')
+
+add= int(num_1) + int(num_2)
+sub= int(num_1) - int(num_2)
+mul= int(num_1) * int(num_2)
+div= int(num_1) / int(num_2)
+
+print(' The Addition = {0} '.format(add))
+print(' The Subtraction = {0} '.format(sub))
+print(' The Multiplication = {0} '.format(mul))
+print(' The Division = {0} '.format(div))
---
+++ swap a munber python.py
@@ -0,0 +1,11 @@
+#swap a munber python
+
+p=int (input("Enter p value =" ))
+q=int (input("Enter q value =" ))
+
+temp=p
+p=q
+q=temp
+
+print(' After Swaping P =',p)
+print(' After Swaping Q =',q)
